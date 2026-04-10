3ee0c2ce11ed9114f5ad699f4177881f3015e6f2
Harshit <heroharshitlaptop@gmail.com>
2026-04-10 11:16:27 +0530

    Slight Redefining Of Code

--- rpsGame.py
+++ rpsGame.py
@@ -8,11 +8,13 @@ time.sleep(1)
 print("You will be playing against the computer.")
 time.sleep(1)
 print("The rules are simple: Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.")
+time.sleep(1)
 print('Press "R" for Rock, "P" for Paper, and "S" for Scissors.')
 time.sleep(1)
-print("Let's get started!")
-rounds = int(input("How many rounds would you like to play ?"))
-
+rounds = int(input("How many rounds would you like to play ? "))
+time.sleep(1)
+print("Great! Let's play ", rounds, "rounds.")
+print("")
 computer_score = 0
 player_score = 0
 
@@ -24,10 +26,10 @@ for round in range(1, rounds + 1):
     if player_choice == computer_choice:
         print("It's a tie!")
     elif (player_choice == "R" and computer_choice == "S") or (player_choice == "P" and computer_choice == "R") or (player_choice == "S" and computer_choice == "P"):
-        print("You win this round!")
+        print("You won this round !")
         player_score += 1
     else:
-        print("Computer wins this round!")
+        print("Computer won this round !")
         computer_score += 1        
 
     print(f"Score for round {round}: Player {player_score} - Computer {computer_score}")
@@ -40,4 +42,3 @@ elif computer_score > player_score:
     print(f"Computer wins the game with a score of {computer_score} to {player_score}. Better luck next time!")
 else:
     print(f"The game is a tie with both scoring {player_score}!")
-
