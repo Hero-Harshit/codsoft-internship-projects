522564cc672927e291691d9010c0e6f222b02ba5
Harshit <heroharshitlaptop@gmail.com>
2026-04-13 01:24:58 +0530

    Minimum Viable Product

--- contactBook.py
+++ contactBook.py
@@ -18,5 +18,33 @@ def display_contacts():
         return
     print("Contact List:")
     for contact in Main_Database:
-        print(f"{contact['name']} {contact['surname']} - {contact['phone']}")    
+        print(f"{contact['name']} {contact['surname']} - {contact['phone']}")
+
+
+print("Welcome to the Contact Book!")
+while True:
+    print("\nOptions:")
+    print("1. Add Contact")
+    print("2. Search Contact")
+    print("3. Display Contacts")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        name = input("Enter name: ")
+        surname = input("Enter surname: ")
+        phone = input("Enter phone number: ")
+        add_contact(name, surname, phone)
+    elif choice == '2':
+        name = input("Enter name to search: ")
+        search_contact(name)
+    elif choice == '3':
+        display_contacts()
+    elif choice == '4':
+        print("Exiting the Contact Book. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
 
